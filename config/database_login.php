b35fac0a1195a86221e80af1025dcc324f830dc1
tim mein <mebusmein@gmail>
2012-10-21 23:39:09 +0200

    login update

--- config/database_login.php
+++ config/database_login.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'root';
+$db_host = '10.0.0.3';
+$db_user = 'tim';
+$db_password = 'systeem';
 $db_database = 'mydb';		
 
 define('DB_HOST',$db_host);
