4a1bc98cff0c9cd6528a5af37e0a9b8a5ffac40e
tim mein <mebusmein@gmail>
2012-10-22 00:10:54 +0200

    html product update

--- product.php
+++ product.php
@@ -14,123 +14,21 @@
             <div class="tile">
               <div class="tile-content image left"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
             </div>
-             <h2 class="place-left">de naam</h2>
-             <div class="place-right" style="padding-right:30px;">
-             <h1 style="line-height:20px; padding-top:20px;">50,00</h1>
-             <small>Saldo</small>
-             </div>
+            <h2 class="place-left">de naam</h2>
+            <div class="place-right" style="padding-right:30px;">
+              <h1 style="line-height:20px; padding-top:20px;">50,00</h1>
+              <small>Saldo</small> </div>
           </div>
-          <div class="tile bg-color-grayDark"> </div>
+          <div class="tile bg-color-grayDark">
+          </div>
         </div>
         <div class="row">
-          <div class="tile selected">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand">
-              <div class="badge bg-color-blue">1</div>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
-          <div class="tile">
-            <div class="tile-content image"> <img src="http://img.perezhilton.com/wp-content/uploads/2010/09/flod5xucjs__oPt.jpg" /> </div>
-            <div class="brand bg-color-orange">
-              <p class="name">dit is een neger</p>
-            </div>
-          </div>
+          <?PHP 
+          	$products = PRODUCT::allProducts();
+			foreach($products as $product){
+				echo $product->photo();
+			}
+			?>
         </div>
       </div>
     </div>
