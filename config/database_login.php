80f187bedd548c2e53f48c7f7fa35d9167da3880
Peter Mein <petermein@gmail.com>
2012-10-22 12:55:43 +0200

    Revert "Added ignore"
    This reverts commit 2611f569ce53aef9c50a73ac28d19541a73af40c.

---
+++ config/database_login.php
@@ -0,0 +1,13 @@
+<?php
+
+$db_host = '10.0.0.3';
+$db_user = 'tim';
+$db_password = 'systeem';
+$db_database = 'mydb';		
+
+define('DB_HOST',$db_host);
+define('DB_USER',$db_user);
+define('DB_PASSWORD',$db_password);
+define('DB_DATABASE',$db_database);
+
+?>
